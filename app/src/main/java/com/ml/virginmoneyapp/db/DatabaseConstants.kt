2caf62da3a1e2e4fa092e5cde832cef61d20214f
DirieD <diriedudez@gmail.com>
2022-09-29 10:22:30 +0100

    v2

--- app/src/main/java/com/ml/virginmoneyapp/db/DatabaseConstants.kt
+++ app/src/main/java/com/ml/virginmoneyapp/db/DatabaseConstants.kt
@@ -2,6 +2,6 @@ package com.ml.virginmoneyapp.db
 
 object DatabaseConstants {
     const val DATABASE_VERSION = 1
-    const val DATABASE_NAME = "reddit_posts.db"
+    const val DATABASE_NAME = "rooms_available.db"
     const val EXPORT_SCHEMA = false
 }
--- app/src/main/java/com/ml/virginmoneyapp/network/NetworkConstants.kt
+++ app/src/main/java/com/ml/virginmoneyapp/network/NetworkConstants.kt
@@ -1,6 +1,6 @@
 package com.ml.virginmoneyapp.network
 
 object NetworkConstants {
-    const val BASE_URL = "https://www.reddit.com/"
-    const val GET_POSTS_ENDPOINT = "r/LiveStreamFails/hot.json"
+    const val BASE_URL = "https://61e947967bc0550017bc61bf.mockapi.io/api/v1"
+    const val GET_POSTS_ENDPOINT = "/rooms"
 }
